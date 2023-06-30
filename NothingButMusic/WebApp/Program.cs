77152679c02357682a3c3369531bb4da6586b77e
Sergey, Butko <s.butko@itransition.com>
2023-06-30 16:09:16 +0300

    Added Serilog support

--- NothingButMusic/WebApp/Program.cs
+++ NothingButMusic/WebApp/Program.cs
@@ -1,15 +1,21 @@
+using Application;
+using Infrastructure;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllersWithViews();
+builder.Services
+    .AddApplication()
+    .AddInfrastructure();
+
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseExceptionHandler("/Error");
     app.UseHsts();
 }
